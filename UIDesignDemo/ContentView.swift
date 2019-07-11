6cb12d071c613eda41c911484dd2a42663c28fb3
wanghongyun <947962910@qq.com>
2019-07-11 16:46:18 +0800

    Initial Commit

---
+++ UIDesignDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  UIDesignDemo
+//
+//  Created by 王洪运 on 2019/7/11.
+//  Copyright © 2019 why. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
