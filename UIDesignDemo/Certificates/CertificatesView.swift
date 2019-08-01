db43a1b5e9c55ab8b527a557f9c30712a817a56d
wanghongyun <947962910@qq.com>
2019-08-01 21:00:44 +0800

    手势

--- UIDesignDemo/Certificates/CertificatesView.swift
+++ UIDesignDemo/Certificates/CertificatesView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct CertificatesView : View {
     
     @State var show = false
+    @State var viewState = CGSize.zero
     
     var rotation3DAxis: (x: CGFloat, y: CGFloat, z: CGFloat) {
         return (x: 10.0, y: 10.0, z: 10.0)
@@ -28,33 +29,45 @@ struct CertificatesView : View {
             
             CardView()
                 .background(Color.red)
-                .offset(x: 0.0, y: show ? -400 : -40)
+                .offset(x: viewState.width, y: viewState.height - (show ? 400 : 40))
                 .scaleEffect(0.85)
                 .rotationEffect(Angle(degrees: show ? 15 : 0))
                 .rotation3DEffect(Angle(degrees: show ? 50 : 0),
                                   axis: rotation3DAxis)
                 .blendMode(.hardLight)
-                .animation(.easeInOut(duration: 0.8))
+                .animation(.easeInOut(duration: 0.6))
             
             CardView()
-                .background(show ? Color.red : Color.blue)
-                .offset(x: 0.0, y: show ? -200 : -20)
+                .background(Color.blue)
+                .offset(x: viewState.width, y: viewState.height - (show ? 200 : 20))
                 .scaleEffect(0.9)
                 .rotationEffect(Angle(degrees: show ? 10 : 0))
                 .rotation3DEffect(Angle(degrees: show ? 40 : 0),
                                   axis: rotation3DAxis)
                 .blendMode(.hardLight)
-                .animation(.easeInOut(duration: 0.8))
+                .animation(.easeInOut(duration: 0.4))
             
             CertificateView()
+                .offset(viewState)
                 .scaleEffect(0.95)
                 .rotationEffect(Angle(degrees: show ? 5 : 0))
                 .rotation3DEffect(Angle(degrees: show ? 30 : 0),
                                   axis: rotation3DAxis)
+                .animation(.spring())
                 .onTapGesture {
                     self.show.toggle()
                 }
-                .animation(.spring())
+                .gesture(
+                    DragGesture()
+                        .onChanged { (value) in
+                            self.viewState = value.translation
+                            self.show = true
+                        }
+                        .onEnded{ (value) in
+                            self.viewState = .zero
+                            self.show = false
+                        }
+                )
             
         }
     }
